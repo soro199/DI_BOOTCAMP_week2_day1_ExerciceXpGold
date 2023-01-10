6f63883bd12db20bbf5b1018d8aaf67bd7ebeb69
seydou soro <w.soro@sectester.io>
2023-01-10 16:11:59 +0000

    Exercice XP Gold

---
+++ Mixup/script.js
@@ -0,0 +1,8 @@
+let str1 = "mix";
+let str2 = "pod";
+console.log(str1.substring(0,2).replace("mi","po"));
+console.log(str1.substring(0,2).replace("po","mi"));
+
+let str3 = str1 + " " + str2
+console.log(typeof(str3))
+console.log(str3);
